884a99b99307ab62675fdd0e39357be0b045765b
Crunchya <crunchya6@gmail.com>
2023-04-03 11:27:05 +0200

    update

--- yapadsou/lib/views/register.dart
+++ yapadsou/lib/views/register.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yapadsou/ui/typographie.dart';
 import 'package:yapadsou/assets/colors/colors.dart';
 
-class SecondPage extends StatelessWidget {
+class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text("Hello World", style: CustomTextStyles.title(
