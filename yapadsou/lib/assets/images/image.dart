21088755601f733cdd46ae8bbdf4d0353c6cef64
demartini luca <luca.demartini@hotmail.fr>
2023-04-03 10:59:29 +0200

    rm étoile + add path image

--- yapadsou/lib/assets/images/image.dart
+++ yapadsou/lib/assets/images/image.dart
@@ -1,3 +1,10 @@
 class CustomImages {
   static String tacos = "lib/assets/images/tacos.png";
+  static String bar = "lib/assets/images/bar.png";
+  static String barathon = "lib/assets/images/barathon.png";
+  static String basicFit = "lib/assets/images/basic_fit.png";
+  static String biliJeu = "lib/assets/images/bili_jeu.png";
+  static String chambre = "lib/assets/images/chambre.png";
+  static String otacos = "lib/assets/images/otacos.png";
+  static String sport = "lib/assets/images/sport.png";
 }
