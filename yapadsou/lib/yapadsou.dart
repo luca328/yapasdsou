e76f772c6eef5403a1a02ab89a84c756c5d86d74
Guilian Bahno <guilian.bahno@laposte.net>
2023-04-03 09:57:46 +0200

    Début arborescense yapasdsou

---
+++ yapadsou/lib/yapadsou.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:yapadsou/views/first_view.dart';
+
+class Yapasdsou extends StatelessWidget {
+  const Yapasdsou({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(body: FirstView()),
+    );
+  }
+}
