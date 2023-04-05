536d8699f7aaed3256640339aef878e91f03d3f7
Crunchya <crunchya6@gmail.com>
2023-04-05 14:07:00 +0200

    update register

--- yapadsou/lib/yapadsou.dart
+++ yapadsou/lib/yapadsou.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yapadsou/assets/colors/colors.dart';
-import 'package:yapadsou/assets/images/image.dart';
 import 'package:yapadsou/views/onboarding.dart';
-import 'package:yapadsou/widgets/widget_card.dart';
+import 'package:flutter/services.dart';
 
 class Yapasdsou extends StatelessWidget {
   const Yapasdsou({super.key});
@@ -10,12 +9,18 @@ class Yapasdsou extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: CustomColors.blue),
         ),
-        home: const Scaffold(resizeToAvoidBottomInset: false, body: Center(child: Onboarding())));
+        home: const Scaffold(
+            resizeToAvoidBottomInset: false,
+            body: Center(child: Onboarding())));
   } //WidgetCard(title: "test titre", subTitle: "test sous titre", brandImage: CustomImages.sport, logo: CustomImages.basicFit)
 }
