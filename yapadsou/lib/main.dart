e84d813b385854e2ca51a6170a7d25891f94695b
demartini luca <luca.demartini@hotmail.fr>
2023-05-22 11:20:54 +0200

    add analytics

--- yapadsou/lib/main.dart
+++ yapadsou/lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:yapadsou/yapadsou.dart';
 
 void main() {
-  runApp(const Yapasdsou());
+  runApp(const Yapasdsou('15e8e4554db289d60f59f16477a2bc62'));
 }
