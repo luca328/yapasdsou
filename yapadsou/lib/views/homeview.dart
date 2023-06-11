8ede0688d31f24d0aef5d66b1fcd68f1f4999d93
demartini luca <luca.demartini@hotmail.fr>
2023-06-11 21:36:02 +0200

    fix

--- yapadsou/lib/views/homeview.dart
+++ yapadsou/lib/views/homeview.dart
@@ -42,123 +42,125 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        alignment: Alignment.centerLeft,
-        color: CustomColors.blue,
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30, top: 30),
-                    child: Text(
-                      'COUCOU TOI',
-                      style: CustomTextStyles.title(
-                          color: CustomColors.brokenWhite),
-                      textAlign: TextAlign.left,
+    return Material(
+      child: Container(
+          alignment: Alignment.centerLeft,
+          color: CustomColors.blue,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30, top: 30),
+                      child: Text(
+                        'COUCOU TOI',
+                        style: CustomTextStyles.title(
+                            color: CustomColors.brokenWhite),
+                        textAlign: TextAlign.left,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30, top: 5),
-                    child: Text(
-                      'T\'es en manque de thunes ?',
-                      style: CustomTextStyles.normalInterText(
-                          color: CustomColors.brokenWhite),
-                      textAlign: TextAlign.left,
-                    ),
-                  )
-                ],
-              ),
-              const Padding(
-                padding: EdgeInsets.all(30),
-                child: CustomSearchBar(
-                  inputKey: "searchBar",
-                  inputText: "Cherche ton bon plan",
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30, top: 5),
+                      child: Text(
+                        'T\'es en manque de thunes ?',
+                        style: CustomTextStyles.normalInterText(
+                            color: CustomColors.brokenWhite),
+                        textAlign: TextAlign.left,
+                      ),
+                    )
+                  ],
                 ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                      color: CustomColors.brokenWhite,
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(30.0),
-                          topRight: Radius.circular(30.0))),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: categories
-                              .map(
-                                (category) => Column(
-                                  children: [
-                                    SimpleIconButton(
-                                        width: 57,
-                                        height: 57,
-                                        pressed: () => {},
-                                        color: CustomColors.getColor(
-                                            category.colorName),
-                                        borderRadius: BorderRadius.circular(10),
-                                        icon: CustomImages.getIcon(
-                                            category.iconName),
-                                        iconSize: 24),
-                                    const SizedBox(
-                                      height: 15,
-                                    ),
-                                    Text(category.title,
-                                        style: CustomTextStyles.normalBoldText(
-                                            color: CustomColors.getColor(
-                                                category.colorName)))
-                                  ],
-                                ),
-                              )
-                              .toList(),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Text(
-                            "LES PLANS DU MOMENTS",
-                            style: CustomTextStyles.normalText(
-                                color: CustomColors.black),
-                            textAlign: TextAlign.left,
+                const Padding(
+                  padding: EdgeInsets.all(30),
+                  child: CustomSearchBar(
+                    inputKey: "searchBar",
+                    inputText: "Cherche ton bon plan",
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                        color: CustomColors.brokenWhite,
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30.0),
+                            topRight: Radius.circular(30.0))),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 15,
                           ),
-                        ),
-                        Wrap(
-                          alignment: WrapAlignment.spaceEvenly,
-                          runAlignment: WrapAlignment.spaceEvenly,
-                          runSpacing: 10,
-                          spacing: 10,
-                          children: plans.map(
-                            (plan) => WidgetCard(
-                              brandImage: CustomImages.getImage(plan.mainImage),
-                              logo: CustomImages.getImage(plan.logo),
-                              subTitle: plan.subtitle,
-                              title: plan.title,
-                              width: 159,
-                              height: 162,
-                              titleSize: 14,
-                              subTitleSize: 10,
-                              imageHeight: 90,
-                              imageWidth: 144,
-                              logoSize: 30,
-                              logoBorderSize: 36
-                            )
-                          ).toList()
-                        ),
-                      ],
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: categories
+                                .map(
+                                  (category) => Column(
+                                    children: [
+                                      SimpleIconButton(
+                                          width: 57,
+                                          height: 57,
+                                          pressed: () => {},
+                                          color: CustomColors.getColor(
+                                              category.colorName),
+                                          borderRadius: BorderRadius.circular(10),
+                                          icon: CustomImages.getIcon(
+                                              category.iconName),
+                                          iconSize: 24),
+                                      const SizedBox(
+                                        height: 15,
+                                      ),
+                                      Text(category.title,
+                                          style: CustomTextStyles.normalBoldText(
+                                              color: CustomColors.getColor(
+                                                  category.colorName)))
+                                    ],
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Text(
+                              "LES PLANS DU MOMENTS",
+                              style: CustomTextStyles.normalText(
+                                  color: CustomColors.black),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                          Wrap(
+                            alignment: WrapAlignment.spaceEvenly,
+                            runAlignment: WrapAlignment.spaceEvenly,
+                            runSpacing: 10,
+                            spacing: 10,
+                            children: plans.map(
+                              (plan) => WidgetCard(
+                                brandImage: CustomImages.getImage(plan.mainImage),
+                                logo: CustomImages.getImage(plan.logo),
+                                subTitle: plan.subtitle,
+                                title: plan.title,
+                                width: 159,
+                                height: 162,
+                                titleSize: 14,
+                                subTitleSize: 10,
+                                imageHeight: 90,
+                                imageWidth: 144,
+                                logoSize: 30,
+                                logoBorderSize: 36
+                              )
+                            ).toList()
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              )
-            ]));
+                )
+              ])),
+    );
   }
 }
